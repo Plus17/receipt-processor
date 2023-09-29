7c0219186b246ff6e5da0a7a3c20b549b6279755
Sergio <me@sergiovp.dev>
2023-09-29 16:02:44 -0600

    feat: adds class to calculate receipt points

---
+++ app/services/receipt_point_calculator.rb
@@ -0,0 +1,52 @@
+class ReceiptPointCalculator
+  def initialize(receipt_data)
+    @receipt = receipt_data
+    @points = 0
+    @bonus_time_start = Time.parse("14:00")
+    @bonus_time_end = Time.parse("16:00")
+  end
+  
+  def calculate_points
+    [
+      retailer_points,
+      total_points,
+      items_points,
+      purchase_date_points,
+      purchase_time_points
+    ].sum
+  end
+  
+  private
+
+  attr_reader :receipt, :bonus_time_start, :bonus_time_end
+
+  def retailer_points
+    receipt["retailer"].scan(/[a-zA-Z0-9]/).count
+  end
+  
+  def total_points
+    total = receipt["total"].to_f
+    points = 0
+    points += 50 if total == total.to_i
+    points += 25 if (total * 100 % 25).zero?
+    points
+  end
+
+  def items_points
+    item_pairs_points = (receipt["items"].size / 2) * 5 # Ruby rounds down for integers by default so no need to ceil
+    description_points = receipt["items"].sum do |item|
+      description = item["shortDescription"].strip
+      (description.length % 3).zero? ? (item["price"].to_f * 0.2).ceil : 0
+    end
+    item_pairs_points + description_points
+  end
+
+  def purchase_date_points
+    purchase_date = Date.parse(receipt["purchaseDate"])
+    purchase_date.day.odd? ? 6 : 0
+  end
+
+  def purchase_time_points
+    Time.parse(receipt["purchaseTime"]).between?(bonus_time_start, bonus_time_end) ? 10 : 0
+  end
+end
---
+++ test/services/receipt_point_calculator_test.rb
@@ -0,0 +1,62 @@
+require "test_helper"
+
+class ReceiptPointCalculatorTest < ActiveSupport::TestCase
+  test "calculates points for a receipt" do
+    receipt_json = '{
+      "retailer": "Target",
+      "purchaseDate": "2022-01-01",
+      "purchaseTime": "13:01",
+      "items": [
+        {
+          "shortDescription": "Mountain Dew 12PK",
+          "price": "6.49"
+        },{
+          "shortDescription": "Emils Cheese Pizza",
+          "price": "12.25"
+        },{
+          "shortDescription": "Knorr Creamy Chicken",
+          "price": "1.26"
+        },{
+          "shortDescription": "Doritos Nacho Cheese",
+          "price": "3.35"
+        },{
+          "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+          "price": "12.00"
+        }
+      ],
+      "total": "35.35"
+    }'
+
+    points_calculator = ReceiptPointCalculator.new(JSON.parse(receipt_json))
+
+    assert_equal 28, points_calculator.calculate_points
+  end
+
+  test "calculates points for a receipt with an odd day" do
+    receipt_json = '{
+      "retailer": "M&M Corner Market",
+      "purchaseDate": "2022-03-20",
+      "purchaseTime": "14:33",
+      "items": [
+        {
+          "shortDescription": "Gatorade",
+          "price": "2.25"
+        },{
+          "shortDescription": "Gatorade",
+          "price": "2.25"
+        },{
+          "shortDescription": "Gatorade",
+          "price": "2.25"
+        },{
+          "shortDescription": "Gatorade",
+          "price": "2.25"
+        }
+      ],
+      "total": "9.00"
+    }'
+
+    points_calculator = ReceiptPointCalculator.new(JSON.parse(receipt_json))
+
+    assert_equal 109, points_calculator.calculate_points
+  end
+end
